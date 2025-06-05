181c0fd821e5dcc6762ef686929e375d39ecc8d6
ranjith <32890982+tkranjit@users.noreply.github.com>
2025-06-05 11:15:12 +0530

    hyper parameter completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -3,7 +3,6 @@ import sys
 
 from src.logger import logging
 from src.exception import CustomException
-from src.utils import evaluate_models
 
 from dataclasses import dataclass
 
@@ -52,20 +51,57 @@ class ModelTrainer:
             )
 
             models = {
-                "RandomForestRegressor": RandomForestRegressor(),
-                "GradientBoostingRegressor": GradientBoostingRegressor(),
-                "AdaBoostRegressor": AdaBoostRegressor(),
-                "LinearRegression": LinearRegression(),
-                "KNeighborsRegressor": KNeighborsRegressor(),
-                "DecisionTreeRegressor": DecisionTreeRegressor(),
-                "CatBoostRegressor": CatBoostRegressor(verbose=0),
-                "XGBRegressor": XGBRegressor()
+                "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest": RandomForestRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=0),
+                "AdaBoost Regressor": AdaBoostRegressor()
+            }
+
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
             }
 
             model_report = {}
 
             model_report = evaluate_models(
-                X_train, y_train, X_test, y_test, models
+                X_train, y_train, X_test, y_test, models,params
             )
 
             best_model_score = max(model_report.values())
@@ -90,6 +126,9 @@ class ModelTrainer:
             logging.info(f"R2 score of the best model: {r2_square}")
             logging.info(f"Best model name: {best_model_name}")
             logging.info(f"Best model score: {best_model_score}")
+            print("model trained and saved successfully")
+            print(f"R2 score of the best model: {r2_square}")
+            print(f"Best model name: {best_model_name}")
             return best_model_name, r2_square
 
         except Exception as e:
